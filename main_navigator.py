f1256cf69cef867659786eddc5a0bb5efaae7885
byronwongdev <byronwongdev@gmail.com>
2020-11-17 00:59:02 +0800

    update with a navigator!

---
+++ main_navigator.py
@@ -0,0 +1,40 @@
+from tkinter import *
+import cv2
+from PIL import Image, ImageTk
+
+root = Tk()
+root.title("opencv")
+root.geometry("600x600")
+
+
+my_entries = []
+
+def projectcar_func():
+    exec(open('projectcar.py').read())
+
+def projectdrawer_func():
+    exec(open('projectdrawer.py').read())
+
+def projectface_func():
+    exec(open('projectface.py').read())
+
+def projectreadcolour_func():
+    exec(open('projectreadcolour.py').read())
+    
+
+
+my_button = Button(root, text="Project Car!", command=projectcar_func)
+my_button.grid(row=1, column=0, pady=20)
+
+my_button = Button(root, text="Project Drawer!", command=projectdrawer_func)
+my_button.grid(row=2, column=0, pady=20)
+
+my_button = Button(root, text="Project Face!", command=projectface_func)
+my_button.grid(row=3, column=0, pady=20)
+
+my_button = Button(root, text="Project Readcolour!", command=projectreadcolour_func)
+my_button.grid(row=4, column=0, pady=20)
+
+
+
+root.mainloop()
